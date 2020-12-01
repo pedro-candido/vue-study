bfb9fc8bfb9fafdd2bb6b86868e3083ae75bf172
Pedro Ferreira Candido <pedro.candido@pampa.compasso>
2020-12-01 17:46:58 -0300

    Some studies in vue

---
+++ firstexample/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+options = {
+    el: '#app',
+    data: {
+        name: 'Pedro Candido'
+    }
+}
+
+const vm = new Vue(options)
+
+console.log(vm)
---
+++ methods/node_modules/bootstrap/js/src/dist/index.dev.js
@@ -0,0 +1,103 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Alert", {
+  enumerable: true,
+  get: function get() {
+    return _alert["default"];
+  }
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _button["default"];
+  }
+});
+Object.defineProperty(exports, "Carousel", {
+  enumerable: true,
+  get: function get() {
+    return _carousel["default"];
+  }
+});
+Object.defineProperty(exports, "Collapse", {
+  enumerable: true,
+  get: function get() {
+    return _collapse["default"];
+  }
+});
+Object.defineProperty(exports, "Dropdown", {
+  enumerable: true,
+  get: function get() {
+    return _dropdown["default"];
+  }
+});
+Object.defineProperty(exports, "Modal", {
+  enumerable: true,
+  get: function get() {
+    return _modal["default"];
+  }
+});
+Object.defineProperty(exports, "Popover", {
+  enumerable: true,
+  get: function get() {
+    return _popover["default"];
+  }
+});
+Object.defineProperty(exports, "Scrollspy", {
+  enumerable: true,
+  get: function get() {
+    return _scrollspy["default"];
+  }
+});
+Object.defineProperty(exports, "Tab", {
+  enumerable: true,
+  get: function get() {
+    return _tab["default"];
+  }
+});
+Object.defineProperty(exports, "Toast", {
+  enumerable: true,
+  get: function get() {
+    return _toast["default"];
+  }
+});
+Object.defineProperty(exports, "Tooltip", {
+  enumerable: true,
+  get: function get() {
+    return _tooltip["default"];
+  }
+});
+Object.defineProperty(exports, "Util", {
+  enumerable: true,
+  get: function get() {
+    return _util["default"];
+  }
+});
+
+var _alert = _interopRequireDefault(require("./alert"));
+
+var _button = _interopRequireDefault(require("./button"));
+
+var _carousel = _interopRequireDefault(require("./carousel"));
+
+var _collapse = _interopRequireDefault(require("./collapse"));
+
+var _dropdown = _interopRequireDefault(require("./dropdown"));
+
+var _modal = _interopRequireDefault(require("./modal"));
+
+var _popover = _interopRequireDefault(require("./popover"));
+
+var _scrollspy = _interopRequireDefault(require("./scrollspy"));
+
+var _tab = _interopRequireDefault(require("./tab"));
+
+var _toast = _interopRequireDefault(require("./toast"));
+
+var _tooltip = _interopRequireDefault(require("./tooltip"));
+
+var _util = _interopRequireDefault(require("./util"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
